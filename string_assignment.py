2b4557b4aed9d8d917ddf83b364e37d2664ad0ed
Irsa Ayaz <irsaayaz.525@gmail.com>
2025-05-13 18:28:38 +0500

    Add files via upload

---
+++ string_assignment.py
@@ -0,0 +1,33 @@
+#Task 1: Characters
+character1 = 'A'
+character2 = '7'
+character3 = '#'
+print(character1, character2, character3) #This code will print 'A, 7 and #'
+
+#Task 2: Strings
+FavAnimal = "Wolfs, Bunny"
+ShortSentence = "Wolfs are strong and I like its apperance, Bunnies are cute and fluffy"
+print(FavAnimal, ShortSentence) #This code will print the value/data inside the string varaible
+
+#Task 3: Concetenation
+first_name = "Irsa"
+last_name = "Ayaz"
+
+full_name = first_name + " " + last_name
+print(full_name) #This code will print 'Irsa Ayaz' (My full name)
+
+#Task 4: Repetition
+greeting = "Hi"
+print(greeting * 5) #This code will print 'Hi' 5 times
+
+#Task 5: Indexing
+coding = "PythonRocks"
+print(greeting[0]) #First Letter
+print(greeting[3]) #Fourth Letter
+print(greeting[-1]) #Last Letter
+
+#Task 6: Slicing
+text = "I love coding in Python"
+love = text.split()
+Python = text.spilt()
+print(love, Python)
